15cff7a8e3326b5d0e9fd4ec6177811816106168
Zekesy <zekesy1208@gmail.com>
2026-02-11 00:39:37 +0900

    Add SDL2 Window and draw simple square

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 
 int main() {
   std::cout << "Hello World\n";
+  
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+      std::cout << "SDL Init Error: " << SDL_GetError() << std::endl;
+      return 1;
+  }
+
+  SDL_Window* window;
+  SDL_Renderer* renderer; 
+
+  int result = SDL_CreateWindowAndRenderer(500, 500, 0, &window, &renderer);
+  if( result != 0 ){
+    std::cout << "Failed to create window and renderer" << SDL_GetError() << std::endl;
+  }
+
+  SDL_SetRenderDrawColor(renderer, 255,255,255,255);
+  SDL_RenderDrawPoint(renderer, 100, 100);
+  SDL_RenderDrawLine( renderer, 10, 10, 50, 25 ); // Draw a line
+
+  SDL_Rect r;
+  r.x = 150;
+  r.y = 25;
+  r.h = 75;
+  r.w = 120;
+
+  SDL_RenderFillRect( renderer, &r ); 
+
+  SDL_RenderPresent (renderer); 
+  
+
+  SDL_Delay(3000);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+
+  SDL_Quit();
+
   return 0; 
 }
 
